084bbdf959160b6583fda229780bd0a06942937e
Beryl Bawo Nene <nenebawoberyl@gmail.com>
2015-04-03 05:03:14 -0400

    last commit

--- gen/com/example/musicplayerapp/R.java
+++ gen/com/example/musicplayerapp/R.java
@@ -66,22 +66,22 @@ public final class R {
         public static final int track_info_bg=0x7f020029;
     }
     public static final class id {
-        public static final int btnBackward=0x7f070008;
-        public static final int btnForward=0x7f07000a;
-        public static final int btnNext=0x7f07000b;
-        public static final int btnPlay=0x7f070009;
-        public static final int btnPlaylist=0x7f070004;
-        public static final int btnPrevious=0x7f070007;
-        public static final int btnRepeat=0x7f070002;
-        public static final int btnShuffle=0x7f070001;
-        public static final int player_footer_bg=0x7f070006;
+        public static final int btnBackward=0x7f070006;
+        public static final int btnForward=0x7f070008;
+        public static final int btnNext=0x7f070009;
+        public static final int btnPlay=0x7f070007;
+        public static final int btnPlaylist=0x7f070002;
+        public static final int btnPrevious=0x7f070005;
+        public static final int btnRepeat=0x7f07000e;
+        public static final int btnShuffle=0x7f07000f;
+        public static final int player_footer_bg=0x7f070004;
         public static final int player_header_bg=0x7f070000;
-        public static final int songCurrentDurationLabel=0x7f07000e;
-        public static final int songProgressBar=0x7f07000c;
-        public static final int songThumbnail=0x7f070005;
-        public static final int songTitle=0x7f070003;
-        public static final int songTotalDurationLabel=0x7f07000f;
-        public static final int timerDisplay=0x7f07000d;
+        public static final int songCurrentDurationLabel=0x7f07000c;
+        public static final int songProgressBar=0x7f07000a;
+        public static final int songThumbnail=0x7f070003;
+        public static final int songTitle=0x7f070001;
+        public static final int songTotalDurationLabel=0x7f07000d;
+        public static final int timerDisplay=0x7f07000b;
     }
     public static final class layout {
         public static final int bg_player_footer=0x7f030000;
